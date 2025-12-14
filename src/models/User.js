d43820d3f2b782a0e3a957873c50d27ff807f5b2
Jim495217 <jimmel.chambers.jr@uhsp.edu>
2025-12-13 18:12:42 -0600

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,21 +1,16 @@
 const { DataTypes } = require("sequelize");
+const sequelize = require("../db/connection");
 
-module.exports = (sequelize) => {
-  const User = sequelize.define("User", {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    passwordHash: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    role: {
-      type: DataTypes.ENUM("user", "collaborator"),
-      defaultValue: "user"
-    }
-  });
+const User = sequelize.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
 
-  return User;
-};
+module.exports = User;
