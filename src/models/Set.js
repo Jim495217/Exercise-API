c7ce0a36ce308e91fa921d446b198c088982d763
Jim495217 <jimmel.chambers.jr@uhsp.edu>
2025-12-13 18:12:25 -0600

    Update Set.js

--- src/models/Set.js
+++ src/models/Set.js
@@ -2,12 +2,10 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../db/connection");
 
 const Set = sequelize.define("Set", {
-  number: {
+  weight: {
     type: DataTypes.INTEGER,
     allowNull: false
   }
-}, {
-  timestamps: true
 });
 
 module.exports = Set;
